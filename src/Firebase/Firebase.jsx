814f9f60f8599366c9706486aa16914f34afb5da
Youssef achehboune <achehbouneyoussef2@gmail.com>
2024-11-14 00:53:01 +0100

    major updates: in this commit i removed api keys that was showed in code and i used the env file to kepp those keys of firebase

--- src/Firebase/Firebase.jsx
+++ src/Firebase/Firebase.jsx
@@ -1,13 +1,15 @@
-import {initializeApp} from "firebase/app"
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAhWyqrEl9Gl8XHlztX5iSqsLHf2C_qEjM",
-    authDomain: "testing-auth-d6a7d.firebaseapp.com",
-    projectId: "testing-auth-d6a7d",
-    storageBucket: "testing-auth-d6a7d.firebasestorage.app",
-    messagingSenderId: "69037439156",
-    appId: "1:69037439156:web:fcf9c288ed467939053fe1"
+    apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+    authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+    projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+    storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+    appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
+    measurementId: `${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app
+const analytics = getAnalytics(app);
